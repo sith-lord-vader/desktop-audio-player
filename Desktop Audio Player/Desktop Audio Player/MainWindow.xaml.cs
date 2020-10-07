03d1f3b640ed6c307380002dbf8446121b04f927
Abhishek Adhikari <abhiayush23@gmail.com>
2020-10-07 21:10:07 +0530

    Project Started

---
+++ Desktop Audio Player/Desktop Audio Player/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Desktop_Audio_Player
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        string filename;
+        public MainWindow() {
+            InitializeComponent();
+        }
+        private void BT_Click_Open(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = false;
+            fileDialog.DefaultExt = ".mp3";
+            bool? dialogOk = fileDialog.ShowDialog();
+            if (dialogOk == true)
+            {
+                filename = fileDialog.FileName;
+                File_Name.Text = filename;
+                mediaPlayer.Open(new Uri(filename));
+            }
+        }
+
+        private void BT_Click_Play(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Play();
+        }
+
+        private void BT_Click_Pause(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Pause();
+        }
+
+        private void BT_Click_Stop(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Stop();
+        }
+    }
+}
