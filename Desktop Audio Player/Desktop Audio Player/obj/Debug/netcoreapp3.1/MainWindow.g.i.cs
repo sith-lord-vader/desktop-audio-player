cfad83b71dedf88a73c68284e53561451b3fe65b
Abhishek Adhikari <abhiayush23@gmail.com>
2020-10-09 13:01:33 +0530

    Explorer .ico added.

--- Desktop Audio Player/Desktop Audio Player/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ Desktop Audio Player/Desktop Audio Player/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -146,7 +146,7 @@ namespace Desktop_Audio_Player {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Desktop Audio Player;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Desktop Audio Player;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
